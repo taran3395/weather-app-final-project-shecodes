70f98b181319e2dd67fda85a0666e4d347f179a4
taran3395 <46003621+taran3395@users.noreply.github.com>
2021-10-06 12:47:04 +0330

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = MAth.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "898479a6582749588bdb0b175b82ac8d";
